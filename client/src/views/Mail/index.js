c3dac569f1c89639c9a770275ab93cc122c8b47a
Khaled Al Najjar <knajjar@mykloud.io>
2022-06-22 13:35:01 +0300

    Resturcting files

--- client/src/views/Mail/index.js
+++ client/src/views/Mail/index.js
@@ -2,35 +2,17 @@ import React from "react";
 import { selectOpenMail } from "../../features/mailSlice";
 import { useSelector } from "react-redux";
 import styles from "./Mail.module.css";
-import MailTool from "./mailTool.js";
+import MailTool from "./mailTool";
+import EditEmail from "./editEmail";
+import ReadEmail from "./readEmail";
 
 function Mail() {
   const selectedMail = useSelector(selectOpenMail);
 
-  let email = {
-    subject: "Hey",
-    sender: "tsar@zhuk.com",
-    recipient: "suleiman@zhukov.com",
-    body: "Hello, tsar",
-    key: "",
-  };
-
   return (
     <div className={styles.mail}>
       <MailTool />
-      <div className={styles["mail-body"]}>
-        <div className={styles["mail-bodyHeader"]}>
-          <div className={styles["mail-subject"]}>
-            <h5>{selectedMail?.subject}</h5>
-            {/* <LabelImportantIcon className={styles["mail-important"]} /> */}
-          </div>
-          <p className={styles["mail-time"]}>{selectedMail?.time}</p>
-        </div>
-
-        <div className={styles["mail-message"]}>
-          <p>{selectedMail?.description}</p>
-        </div>
-      </div>
+      {selectedMail ? <ReadEmail /> : <EditEmail />}
     </div>
   );
 }
---
+++ client/src/views/Mail/readEmail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { selectOpenMail } from "../../features/mailSlice";
+import { useSelector } from "react-redux";
+import styles from "./Mail.module.css";
+
+function ReadEmail() {
+  const selectedMail = useSelector(selectOpenMail);
+
+  return (
+    <div className={styles["mail-body"]}>
+      <div className={styles["mail-bodyHeader"]}>
+        <div className={styles["mail-subject"]}>
+          <h5>{selectedMail.subject}</h5>
+          {/* <LabelImportantIcon className={styles["mail-important"]} /> */}
+        </div>
+        <p className={styles["mail-time"]}>{selectedMail.time}</p>
+      </div>
+
+      <div className={styles["mail-message"]}>
+        <p>{selectedMail.description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ReadEmail;
