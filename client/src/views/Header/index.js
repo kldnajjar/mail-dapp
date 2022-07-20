248d6a2389d83cf0039925227fe10bba63ecc5f7
Khaled Al Najjar <knajjar@mykloud.io>
2022-07-20 16:10:55 +0300

    refactoring header

--- client/src/views/Header/index.js
+++ client/src/views/Header/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
 import MenuIcon from "@material-ui/icons/Menu";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
@@ -7,20 +10,20 @@ import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import { useSelector, useDispatch } from "react-redux";
+
 import { selectUser, logout } from "../../features/userSlice";
-import { useNavigate } from "react-router-dom";
 import useGunContext from "../../context/useGunContext";
 import useSessionChannel from "../../hooks/useSessionChannel";
+
 import logo from "../../aseets/logo.png";
 import styles from "./Header.module.css";
 
 function Header() {
-  const user = useSelector(selectUser);
-  const { getUser } = useGunContext();
-  const sessionChannel = useSessionChannel();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const sessionChannel = useSessionChannel();
+  const { getUser } = useGunContext();
 
   const signOut = (evt) => {
     sessionStorage.removeItem("account");
