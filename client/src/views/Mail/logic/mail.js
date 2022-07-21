e6b6fc874bdfdfec458a47815c5cc243d753b98b
Khaled Al Najjar <knajjar@mykloud.io>
2022-07-21 16:48:02 +0300

    Adding more refactoring

--- client/src/views/Mail/logic/mail.js
+++ client/src/views/Mail/logic/mail.js
@@ -1,23 +1,24 @@
 import { encryption } from "../../../util/privacy";
-import { closeSendMessage } from "../../../slices/mailSlice";
-import { toast } from "react-toastify";
-import Gun from "gun/gun";
+import {
+  getMailEmails,
+  handleConversationAndMessages,
+  createMessagesWithRelatedConversation,
+} from "./send";
 
 export const createEmail = async (emailObject, context) => {
+  const { getGun, getUser } = context;
+
   const emailsArray = getMailEmails(emailObject);
 
-  const emailEncrypted = await encryption(
-    {
-      subject: emailObject.subject,
-      sender: emailObject.sender,
-      recipients: emailsArray.recipientsArray,
-      body: emailObject.body,
-      cc: emailsArray.carbonCopyArray,
-      bcc: emailsArray.blindCarbonCopyArray,
-    },
-    context.getGun,
-    context.getUser
-  );
+  const obj = {
+    subject: emailObject.subject,
+    sender: emailObject.sender,
+    body: emailObject.body,
+    recipients: emailsArray.recipientsArray,
+    cc: emailsArray.carbonCopyArray,
+    bcc: emailsArray.blindCarbonCopyArray,
+  };
+  const emailEncrypted = await encryption(obj, getGun, getUser);
 
   const msgObj = await handleConversationAndMessages(
     emailObject,
@@ -25,120 +26,10 @@ export const createEmail = async (emailObject, context) => {
     emailsArray
   );
 
-  createMessagesWithRelatedConversation(context, msgObj, emailObject);
-};
-
-const getMailEmails = (obj) => {
-  const recipientsArray = obj.recipient.split(";");
-
-  let carbonCopyArray;
-  if (obj.cc.length) {
-    carbonCopyArray = obj.cc.split(";");
-  }
-
-  let blindCarbonCopyArray;
-  if (obj.bcc.length) {
-    blindCarbonCopyArray = obj.bcc.split(";");
-  }
-
-  return {
-    recipientsArray,
-    carbonCopyArray,
-    blindCarbonCopyArray,
-  };
-};
-
-const handleConversationAndMessages = (
-  emailObject,
-  emailEncrypted,
-  emailsArray
-) => {
-  const jsonObj = JSON.stringify(emailEncrypted?.encryptedUsersKeys);
-  const carbonCopyJsonObj = JSON.stringify(emailsArray.carbonCopyArray);
-  const blindCarbonCopyJsonObj = JSON.stringify(
-    emailsArray.blindCarbonCopyArray
+  createMessagesWithRelatedConversation(
+    context,
+    msgObj,
+    emailObject,
+    emailsArray
   );
-
-  const conversationObj = {};
-  const messageObj = {};
-
-  conversationObj.id = emailObject.conversationId;
-  conversationObj.recentBody = emailEncrypted?.encryptedMessage;
-
-  messageObj.id = emailObject.messageId;
-  messageObj.type = emailObject.messageType;
-  messageObj.body = emailEncrypted?.encryptedMessage;
-  messageObj.sender = emailObject?.sender;
-  messageObj.recipients = emailObject?.recipient;
-  messageObj.timestamp = Gun.state();
-
-  if (messageObj.type !== "reply") {
-    conversationObj.subject = emailEncrypted?.encryptedSubject;
-    conversationObj.keys = jsonObj;
-    conversationObj.sender = emailObject?.sender;
-    conversationObj.senderEpub = emailEncrypted?.senderEpub;
-    conversationObj.cc =
-      typeof carbonCopyJsonObj === "undefined" ? "" : carbonCopyJsonObj;
-    conversationObj.bcc =
-      typeof blindCarbonCopyJsonObj === "undefined"
-        ? ""
-        : blindCarbonCopyJsonObj;
-
-    messageObj.carbonCopy = emailObject?.cc;
-    messageObj.blindCarbonCopy = emailObject?.bcc;
-  }
-
-  const newRecipientArray = emailsArray.recipientsArray.concat(
-    emailsArray.carbonCopyArray,
-    emailsArray.blindCarbonCopyArray
-  );
-
-  return {
-    conversationObj,
-    messageObj,
-    newRecipientArray,
-  };
-};
-
-const createMessagesWithRelatedConversation = async (
-  context,
-  mailObj,
-  emailObject
-) => {
-  await context
-    .getMails()
-    .get(mailObj.conversationObj.id)
-    .put(mailObj.conversationObj)
-    .get("messages")
-    .get(mailObj.messageObj.id)
-    .put(mailObj.messageObj);
-
-  const conversation = context.getMails().get(mailObj.conversationObj.id);
-
-  console.log(emailObject.sender);
-  console.log(mailObj.newRecipientArray);
-  context
-    .getGun()
-    .get("accounts")
-    .get(emailObject.sender)
-    .get("folders")
-    .get("sent")
-    .set(conversation);
-
-  for (let i = 0; i < mailObj.newRecipientArray.length; i++) {
-    console.log("before if statement", mailObj.newRecipientArray[i]);
-    if (typeof mailObj.newRecipientArray[i] !== "undefined") {
-      console.log("after if statement", mailObj.newRecipientArray[i]);
-      context
-        .getGun()
-        .get("accounts")
-        .get(mailObj.newRecipientArray[i])
-        .get("folders")
-        .get("inbox")
-        .set(conversation);
-    }
-  }
-
-  context.dispatch(closeSendMessage());
-  toast.success("Email sent");
 };
---
+++ client/src/views/Mail/logic/send.js
@@ -0,0 +1,127 @@
+import { closeSendMessage } from "../../../slices/mailSlice";
+import { toast } from "react-toastify";
+import Gun from "gun/gun";
+
+const getMailEmails = (obj) => {
+  const recipientsArray = obj.recipient.split(";");
+
+  let carbonCopyArray;
+  if (obj.cc.length) {
+    carbonCopyArray = obj.cc.split(";");
+  }
+
+  let blindCarbonCopyArray;
+  if (obj.bcc.length) {
+    blindCarbonCopyArray = obj.bcc.split(";");
+  }
+
+  const newRecipientArray = recipientsArray.concat(
+    carbonCopyArray,
+    blindCarbonCopyArray
+  );
+
+  return {
+    recipientsArray,
+    carbonCopyArray,
+    blindCarbonCopyArray,
+    newRecipientArray,
+  };
+};
+
+const handleConversationAndMessages = (
+  emailObject,
+  emailEncrypted,
+  emailsArray
+) => {
+  const { conversationId, messageId, messageType, sender, recipient, cc, bcc } =
+    emailObject;
+  const { encryptedUsersKeys, encryptedMessage, encryptedSubject, senderEpub } =
+    emailEncrypted;
+  const { carbonCopyArray, blindCarbonCopyArray } = emailsArray;
+
+  const jsonObj = JSON.stringify(encryptedUsersKeys || "");
+  const carbonCopyJsonObj = JSON.stringify(carbonCopyArray);
+  const blindCarbonCopyJsonObj = JSON.stringify(blindCarbonCopyArray);
+
+  const conversationObj = {};
+  const messageObj = {};
+
+  conversationObj.id = conversationId;
+  conversationObj.recentBody = encryptedMessage || "";
+
+  messageObj.id = messageId;
+  messageObj.type = messageType;
+  messageObj.body = encryptedMessage || "";
+  messageObj.sender = sender || "";
+  messageObj.recipients = recipient || "";
+  messageObj.timestamp = Gun.state();
+
+  if (messageObj.type === "reply") {
+    return {
+      conversationObj,
+      messageObj,
+    };
+  }
+
+  conversationObj.subject = encryptedSubject || "";
+  conversationObj.keys = jsonObj;
+  conversationObj.senderEpub = senderEpub || "";
+  conversationObj.cc = carbonCopyJsonObj ? carbonCopyJsonObj : "";
+  conversationObj.bcc = blindCarbonCopyJsonObj ? blindCarbonCopyJsonObj : "";
+
+  messageObj.carbonCopy = cc || "";
+  messageObj.blindCarbonCopy = bcc || "";
+
+  return {
+    conversationObj,
+    messageObj,
+  };
+};
+
+const createMessagesWithRelatedConversation = async (
+  context,
+  mailObj,
+  emailObject,
+  emailsArray
+) => {
+  const { sender } = emailObject;
+  const { newRecipientArray } = emailsArray;
+  const { conversationObj, messageObj } = mailObj;
+  const { getGun, getMails, dispatch } = context;
+
+  await getMails()
+    .get(conversationObj.id)
+    .put(conversationObj)
+    .get("messages")
+    .get(messageObj.id)
+    .put(messageObj);
+
+  const conversation = getMails().get(conversationObj.id);
+
+  getGun()
+    .get("accounts")
+    .get(sender)
+    .get("folders")
+    .get("sent")
+    .set(conversation);
+
+  for (let i = 0; i < newRecipientArray.length; i++) {
+    if (newRecipientArray[i]) {
+      getGun()
+        .get("accounts")
+        .get(newRecipientArray[i])
+        .get("folders")
+        .get("inbox")
+        .set(conversation);
+    }
+  }
+
+  dispatch(closeSendMessage());
+  toast.success("Email sent");
+};
+
+export {
+  getMailEmails,
+  handleConversationAndMessages,
+  createMessagesWithRelatedConversation,
+};
